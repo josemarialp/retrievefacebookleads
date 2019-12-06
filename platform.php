bd589d4669f9971ff84bc1cad6d4f3e550103079
José María <josemaria.delpozocastrillo@gmail.com>
2019-12-06 21:51:38 +0000

    Update platform.php

--- platform.php
+++ platform.php
@@ -6,6 +6,20 @@
 </head>
 <body>
 <script>
+    
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
+  }    
+    
+    
+    
 function checkLoginState() {               // Called when a person is finished with the Login Button.
     FB.getLoginStatus(function(response) {   // See the onlogin handler
       statusChangeCallback(response);
