db0ecd2e1b60a2a4113121b7ed9dab15322ccdd0
José María <josemaria.delpozocastrillo@gmail.com>
2019-12-06 19:04:17 +0000

    Create platform.php

---
+++ platform.php
@@ -0,0 +1,12 @@
+<h2>My platform</h2>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '581731892589617',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v5.0'
+    });
+  };
+</script>
+<script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>
