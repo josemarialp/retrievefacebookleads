d808f3d774e31a073561064a929366e170626583
José María <josemaria.delpozocastrillo@gmail.com>
2019-12-08 21:24:28 +0000

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -9,4 +9,15 @@ echo $challenge;
 
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
+$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
+
+//Get the lead info
+$lead = getLead($leadgen_id);//get lead info
+//Create an email with the lead info
+<html><body><h2>New lead</h2><blockquote>";
+$mail.="Lead id: ".$leadgen_id."<br>";
+foreach($lead as $attr=>$val) {
+    $mail.=$attr.": ".$val."<br>";
+}
+$mail.="</blockquote></body></html>";
 ?>
